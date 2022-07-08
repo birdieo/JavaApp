83f9d9e70d0cc27f82d2644353ed3dbf58f8a048
Petepycy <PIOTRCICHOCKI262@GMAIL.COM>
2022-07-08 17:44:22 +0200

    wybor 5

--- businesstrip/src/App/Gui.java
+++ businesstrip/src/App/Gui.java
@@ -1,2 +1,18 @@
-package App;public class Gui {
+package App;
+
+import javax.swing.JOptionPane;
+
+public class Gui {
+
+        public static void main(String[] a) {
+            String[] choices = { "A", "B", "C", "D", "E", "F" };
+            String input = (String) JOptionPane.showInputDialog(null, "Choose now...",
+                    "The Choice of a Lifetime", JOptionPane.QUESTION_MESSAGE, null, // Use
+                    // default
+                    // icon
+                    choices, // Array of choices
+                    choices[1]); // Initial choice
+            System.out.println(input);
+
+    }
 }
